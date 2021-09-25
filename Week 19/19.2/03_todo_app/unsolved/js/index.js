34cea096e85075a0a1480be2319dbd7afcd3f323
Karla M Perez <karla.92la@hotmail.com>
2021-09-25 10:29:42 -0400

    19.3

---
+++ Week 19/19.2/03_todo_app/unsolved/js/index.js
@@ -0,0 +1,29 @@
+// Create event listener to enter items below
+$("#enter").on("click", function() {
+
+    var task=$("#todoItem").val();
+    $("#todoList").append("<div class='task'>" + task + "<div class='x fas fa-times'></div></div>");
+
+
+});
+// Create event listener to delete items below
+$(document).on("click"," .x", function() {
+     $(this).parent().remove();
+
+
+
+
+});
+
+// Create event listener to toggle 
+
+$(document).on("click",".task", function() {
+    $(this).toggleClass("donde");
+
+
+    
+
+
+
+
+});
